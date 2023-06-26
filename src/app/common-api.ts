d20c6d79cc1feed5afb96c82140817b423af7b6d
Hanna A. Slivankova <annslivankova@gmail.com>
2023-06-26 23:52:46 +0300

    fix: changed baseURL for local build

--- src/app/common-api.ts
+++ src/app/common-api.ts
@@ -1,12 +1,25 @@
 import axios from 'axios'
 
+// export const instance = axios.create({
+// 	baseURL: process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000' : 'https://bulbashfood.ru'
+// })
+//
+// const token = localStorage.getItem('token')
+// export const instanceUser = axios.create({
+// 	baseURL: process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000' : 'https://bulbashfood.ru',
+// 	// withCredentials: true,
+// 	headers: {
+// 		Authorization: `Bearer ${token}`
+// 	}
+// })
+
 export const instance = axios.create({
-	baseURL: process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000' : 'https://bulbashfood.ru'
+	baseURL: process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000' : 'http://0.0.0.0:8000'
 })
 
 const token = localStorage.getItem('token')
 export const instanceUser = axios.create({
-	baseURL: process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000' : 'https://bulbashfood.ru',
+	baseURL: process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000' : 'http://0.0.0.0:8000',
 	// withCredentials: true,
 	headers: {
 		Authorization: `Bearer ${token}`
