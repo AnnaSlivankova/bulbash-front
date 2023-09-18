e66e3b061cfd1b07402f39611ef1ef7347409a6e
Hanna A. Slivankova <annslivankova@gmail.com>
2023-09-18 13:19:14 +0300

    fix: changed request path in common api

--- src/app/common-api.ts
+++ src/app/common-api.ts
@@ -1,12 +1,13 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-	baseURL: process.env.NODE_ENV !== 'development' ? 'http://0.0.0.0:8000' : 'https://bulbashfood.ru'
+	// baseURL: process.env.NODE_ENV !== 'development' ? 'http://0.0.0.0:8000' : 'https://bulbashfood.ru'
+	baseURL: process.env.NODE_ENV !== 'development' ? 'https://bulbashfood.ru' : 'https://bulbashfood.ru'
 })
 
 const token = localStorage.getItem('token')
 export const instanceUser = axios.create({
-	baseURL: process.env.NODE_ENV !== 'development' ? 'http://0.0.0.0:8000' : 'https://bulbashfood.ru',
+	baseURL: process.env.NODE_ENV !== 'development' ? 'https://bulbashfood.ru' : 'https://bulbashfood.ru',
 	// withCredentials: true,
 	headers: {
 		Authorization: `Bearer ${token}`
